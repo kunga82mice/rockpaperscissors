fd72bcbce3e8a8b70476ec0081fd2100798b0b9d
kunga82mice <kunga82mice@gmail.com>
2019-11-25 23:04:02 +0000

    halfway_done_with_project

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 $("#shoot").click(function(){
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+var computerchoice = "";
+if (randomNumber >= 0.0 && randomNumber <= 0.33)
+   { 
+        computerchoice = "rock";
+   } 
+else if(randomNumber >0.33 && randomNumber <=0.66)
+   {
+       computerchoice = "paper";
+   }
 
-    let message = Math.random();
-    console.log(message);
+else if (randomNumber >0.66 && randomNumber <=1.0)
+   {
+      computerchoice = "scissor";
+    }
+console.log(computerchoice);
+
 });
- 
